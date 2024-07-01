13402524a340618effe547dd60b9cb4a2b588a95
Augusto Brito <britoaugusto1@gmail.com>
2024-07-01 10:12:35 -0300

    se agregaro codigo a 01-variables

--- js/01-variables.js
+++ js/01-variables.js
@@ -1,1 +1,39 @@
 // Esto es un comentario de una linea
+
+/* Esto es un comentario 
+de
+multiples
+lineas
+*/
+
+// declarar una variable (var- let - const)
+
+ let anioActual = 2024;
+ console.log(anioActual)
+
+ // modificamos el contenido de una variable.
+
+ anioActual = 2024 + 2;
+ console.log(anioActual)
+ document.write(anioActual)
+  document.write("<p class= 'textoResaltado'>Esto es un parrafo</p>")
+  document.write('Año actual: '+ [anioActual - 2]
+  )
+
+  // ventanas emergentes
+
+  // alert('Hola mundo')
+
+  const url = 'https://universe.rollingcodeschool.com/my-courses'
+  
+  document.write('<br>'+url)
+
+  // pedir al usuario un numero 
+
+  const numero1 = parseInt(prompt('Ingresa un número'));
+  console.log(numero1)
+  const resultado = numero1 + 20;
+console.log(resultado)
+
+// document.write("<h2>Resultado: " + resultado + "  </h2>")
+document.write(`<h2>Resultado: ${resultado} </h2>`);
