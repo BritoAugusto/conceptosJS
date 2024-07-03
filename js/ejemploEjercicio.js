4e0bc2922fca5d75fb3bcfc5d264f8364742f65d
Augusto Brito <britoaugusto1@gmail.com>
2024-07-03 12:19:53 -0300

    se guardo un cambio

---
+++ js/ejemploEjercicio.js
@@ -0,0 +1,10 @@
+let frase = prompt('ingrese una frase')
+console.log(frase.length)
+
+frase = frase.toLowerCase();
+
+for(let i = 0; i < frase.length; i++ ){
+    if (frase.charAt(i) === 'a' || frase.charAt(i) === 'e' || frase.charAt(i) === 'i' || frase.charAt(i) === 'o' || frase.charAt(i) === 'u') {
+        document.write(frase.charAt(i))
+    }
+}
