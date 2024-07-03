4ec7d77c354ee199719b4c5209ade62740129f63
Augusto Brito <britoaugusto1@gmail.com>
2024-07-03 10:36:01 -0300

    se agrego un ejemplo de bucle while

---
+++ js/03-bucles.js
@@ -0,0 +1,21 @@
+// estructuras repetitivas
+
+//estructura while
+
+/*
+while(condicion logica){
+aqui agrego todo el codigo que quiero repetir
+agregar un codigo para que la condicion logica deje de repetirse
+}
+
+*/
+let renglon = 1;
+
+while (renglon <= 10) {
+    document.write(`<p>Renglon número ${renglon}</p>`)
+    renglon = renglon + 1;
+}
+
+//estructura do while
+
+//estructura for
