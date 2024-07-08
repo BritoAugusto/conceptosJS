821ff43e0c4b44d8bd247a8fd10938cf7e68cd43
Augusto Brito <britoaugusto1@gmail.com>
2024-07-08 11:21:41 -0300

    comiteamos el numero de filas que pide el usuario

--- js/ejemploEjercicio.js
+++ js/ejemploEjercicio.js
@@ -44,9 +44,11 @@ const columnas = parseInt(prompt('Ingrese un número de columnas'))
             
 document.write(`<table>
             <tbody>`)
+    for(let indiceFilas = 1; indiceFilas <= filas; indiceFilas++){      
   document.write(`<tr>`)
-for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++)
+for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
     document.write(`<td>1</td>`)
-
+}
     document.write(`</tr>`) 
+    }
 document.write(`</tbody> </table>`)
