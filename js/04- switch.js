75ea51b42264eaf08f0ff53631e4a717345a315c
Augusto Brito <britoaugusto1@gmail.com>
2024-07-10 10:06:20 -0300

    agregamos cambios en switch.js

--- js/04- switch.js
+++ js/04- switch.js
@@ -1,25 +1,27 @@
 // numero 1 - domingo hasta el 7 = sabado
 
-const dia = parseInt(prompt('Ingresa el numero del dia'))
-
-// if(dia === 1 ){
-//     document.write('Hoy es domingo')
-// }else if (dia === 2) {
-//     document.write
-// }
-
+do {
+let dia = prompt('Ingresa el numero del dia');
+console.log(new Date())
+dia = dia.toLowerCase(); // aqui transformo en minuscula
+// dia = dia.toUpperCase(); Aqui transfromo en mayuscula
 switch (dia) {
-    case 1:
+    case "1":
+    case "domingo":
          document.write('Hoy es domingo');
+         console.log('Hoy es domingo');
     break;
-     case 2:
+     case "2":
          document.write('Hoy es Lunes');
+         console.log('Hoy es lunes');
          break;
-         case 3:
+         case "3":
          document.write('Hoy es Martes');
+         console.log('Hoy es Martes');
          break;
 
          default:
             document.write('Ingresaste un valor invalido')
 
 }
+}while(confirm('¿Desea Continuar?'))
